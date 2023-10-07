ded899b33bfe204045182f6429f508a5054200df
giancarlo valentin <giancarlo.7991@gmail.com>
2023-10-07 16:37:18 -0500

    add cors configuration for spring security

--- src/main/java/com/app/core/security/SecurityConfig.java
+++ src/main/java/com/app/core/security/SecurityConfig.java
@@ -1,8 +1,11 @@
 package com.app.core.security;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -10,6 +13,9 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import lombok.RequiredArgsConstructor;
 
@@ -21,43 +27,50 @@ import lombok.RequiredArgsConstructor;
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-	
-	private static final String[] WHITE_LIST_URL = {"/api/v1/auth/**",
-            "/v2/api-docs",
-            "/v3/api-docs",
-            "/v3/api-docs/**",
-            "/swagger-resources",
-            "/swagger-resources/**",
-            "/configuration/ui",
-            "/configuration/security",
-            "/swagger-ui/**",
-            "/webjars/**",
-            "/swagger-ui.html"};
-	
+
+	private static final String[] WHITE_LIST_URL = { "/api/v1/auth/**", "/v2/api-docs",
+			"/v3/api-docs", "/v3/api-docs/**", "/swagger-resources", "/swagger-resources/**", "/configuration/ui",
+			"/configuration/security", "/swagger-ui/**", "/webjars/**", "/swagger-ui.html" };
+
 	private final JwtAuthenticationFilter jwtAuthenticationFilter;
 	private final AuthenticationProvider authenticationProvider;
 	private final LogoutHandler logoutHandler;
-	
+
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-		return httpSecurity
-				.csrf(csrf -> {
-					csrf.disable();
-				})
-				.authorizeHttpRequests(authResquest -> {
-					authResquest.requestMatchers(WHITE_LIST_URL).permitAll();
-					//authResquest.requestMatchers("/api/v1/user/**").hasAnyRole(USER.name());
-					//authResquest.requestMatchers(GET, "/api/v1/management/**").hasAnyAuthority(ADMIN_READ.name(), MANAGER_READ.name());
-					authResquest.anyRequest().authenticated();					
-				})
-				.sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+		return httpSecurity.csrf(csrf -> {
+			csrf.disable();
+		}).authorizeHttpRequests(authResquest -> {
+			authResquest.requestMatchers(WHITE_LIST_URL).permitAll();
+			// authResquest.requestMatchers("/api/v1/user/**").hasAnyRole(USER.name());
+			// authResquest.requestMatchers(GET,
+			// "/api/v1/management/**").hasAnyAuthority(ADMIN_READ.name(),
+			// MANAGER_READ.name());
+			authResquest.anyRequest().authenticated();
+		}).sessionManagement(
+				sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				.authenticationProvider(authenticationProvider)
-				.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-				.logout(logout -> {
+				.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class).logout(logout -> {
 					logout.logoutUrl("/api/v1/auth/logout");
 					logout.addLogoutHandler(logoutHandler);
-					logout.logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext());
+					logout.logoutSuccessHandler(
+							(request, response, authentication) -> SecurityContextHolder.clearContext());
 				})
+				 .cors(Customizer.withDefaults())
 				.build();
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS", "HEAD"));
+		configuration.setAllowCredentials(true);
+		configuration.setAllowedHeaders(Arrays.asList("Authorization", "Requestor-Type"));
+		configuration.setExposedHeaders(Arrays.asList("X-Get-Header"));
+		configuration.setMaxAge(3600L);
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 }
